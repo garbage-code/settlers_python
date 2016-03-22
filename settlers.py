52280634186ce1a1ad3c88f2316803fe9bf9c7fd
Mike Aspinwall <startrekrules4@gmail.com>
2016-03-22 16:22:44 -0400

    Player_Start - First commit

---
+++ settlers.py
@@ -0,0 +1,33 @@
+#Settlers of Catan
+def play():
+    input1 = input("How Many Players?:   ")
+    return input1
+
+playernames = [] 
+           
+def playerinput(string):
+    global playernames
+    if int(string) == 1:
+        playernames.append(input("Enter Player " + string + " Name:   "))
+    else:
+        playernames.append(input("Enter Player " + string + " Name:   "))
+        playerinput(str(int(string) - 1))
+        
+        
+        
+
+    
+        
+class Player:
+    
+    def __init__(self, name, color, position):
+        self.name = name
+        self.color = color
+        self.position = position
+        
+    def isturn(self, turn): #Global turn must be implemented
+        if self.position == turn:
+            return True
+        else:
+            return False
+
