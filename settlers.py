3732f44df0110197cf1acd847aefc456cb45beda
cmckayepp <cbmckayepp@un004-imini05.local>
2016-03-29 15:21:05 -0400

    Update
    More complete player input

--- settlers.py
+++ settlers.py
@@ -2,6 +2,17 @@
 import random
 import pygame
 
+
+dice1 = 0
+dice2 = 0
+
+def roll():
+    global dice1
+    global dice2
+    dice1 = random.randint(2, 7)
+    dice2 = random.randint(7, 12)
+    
+    
 types = ["bread", "sauce", "cheese", "ham", "pepperoni"]
 trigger = [dice1, dice2]
 
@@ -9,25 +20,46 @@ trigger = [dice1, dice2]
 
 def play():
     input1 = input("How Many Players?:   ")
-    return input1
+    numplayers = input1
+    return str(input1)
 
+numplayers = 0
+colors = ['red', 'blue', 'white', 'orange', 'green', 'brown']
 playernames = [] 
+playercolor = [] 
+playerposition = []
+
+
            
 def playerinput(string):
     global playernames
+    global colors
     if int(string) == 1:
         playernames.append(input("Enter Player " + string + " Name:   "))
+        m = str(input("Enter Player " + string + " Color (Blue, Red, White, Orange, Green, or Brown):   ")).lower()
+        while colors.count(m) != 1 or playercolor.count(m) > 0:
+            m = str(input("Enter Player " + string + " Color (Blue, Red, White, Orange, Green, or Brown):   ")).lower()
+        playercolor.append(m) 
+        playerposition.append(int(string))
     else:
         playernames.append(input("Enter Player " + string + " Name:   "))
+        m = str(input("Enter Player " + string + " Color (Blue, Red, White, Orange, Green, or Brown):   ")).lower()
+        while colors.count(m) != 1 or playercolor.count(m) > 0:
+            m = str(input("Enter Player " + string + " Color (Blue, Red, White, Orange, Green, or Brown):   ")).lower()
+        playercolor.append(m) 
+        playerposition.append(int(string))
         playerinput(str(int(string) - 1))
         
 class Player:
     
-    def __init__(self, name, color, position, cards, cities, roads):
+    def __init__(self, name, color, position, points = 0):
         self.name = name
         self.color = color
         self.position = position
-        self.items = {}
+        self.points = points
+        
+    #def display():
+     #   print(self.name = name, self.color = color, self.position = position, self.points = points)
         
     def isturn(self, turn): #Global turn must be implemented
         if self.position == turn:
@@ -38,12 +70,9 @@ class Player:
     def add_item(self, item):
         self.items[item.name] = item
 
-class Card:
-    def __init__(self, resource, 
+#class Card:
+ #   def __init__(self, resource, 
 
-def roll():
-    dice1 = random.randint(2, 7)
-    dice2 = random.randint(7, 12)
 
 class Tile:
 
@@ -51,6 +80,16 @@ class Tile:
     def __init__(self, type, trigger):
         self.type = type
         self.trigger = trigger
+        
+
+#def init():
+ #   for i in numplayers:
+   #     player + i  = Player(playernames(i), playercolor(i), playerposition(i))
+        
+playerinput(play())
+
+
+
 """
 class ResourceCard:
 
